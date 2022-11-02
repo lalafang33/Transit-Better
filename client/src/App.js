9a21af6135753f415a48d98703f743babf9a4be9
Lalafang33 <lalafang33@gmail.com>
2022-11-02 15:56:29 -0700

    intial move from backend to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
       <h2> Hello </h2>
-     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }
