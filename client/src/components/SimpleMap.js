2f6d92ad77b3df6f191333c8b6fbe71718462a8d
Lalafang33 <lalafang33@gmail.com>
2022-11-03 15:51:50 -0700

    updating simplemap, deletion of marker

--- client/src/components/SimpleMap.js
+++ client/src/components/SimpleMap.js
@@ -53,15 +53,9 @@ export default function SimpleMap(props){
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
         ref={inputEl}
-        bootstrapURLKeys={{ key: "API KEY" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
       </GoogleMapReact>
       <DirectionForm
         map={inputEl}
