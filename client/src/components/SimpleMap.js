517e71e8f4f1897b33d43a88965fdcfee8e7d6ab
Michael Muirhead <michael.muirhead123@gmail.com>
2022-11-08 19:29:08 -0800

    Security Fix

--- client/src/components/SimpleMap.js
+++ client/src/components/SimpleMap.js
@@ -82,7 +82,7 @@ export default function SimpleMap(props) {
       <GoogleMapReact
         id="google-map"
         style={{ height: '100vh', width: 'this.state.progress', position: 'relative' }}
-        bootstrapURLKeys={{ key: "AIzaSyBVAn2esW_R4JPSRFLH1oyT5ppy91l-ECc" }}
+        bootstrapURLKeys={{ key: "" }}
         ref={inputEl}
         center={ {lat: props.userLat, lng: props.userLong } || { lat: 49.2712, lng: -123.1340 }}
         zoom={zoom}
